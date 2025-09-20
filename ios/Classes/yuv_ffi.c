ca0137b3d49656789c8fa8055fa63aba78392642
Oleg Toplionkin <oleg.toplionkin@gmail,com>
2025-09-20 23:10:07 +0300

    - ios refix

--- ios/Classes/yuv_ffi.c
+++ ios/Classes/yuv_ffi.c
@@ -1,9 +1,6 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../{projectName}}.podspec for more information.
-//#include "../../src/yuv/yuv.h"
-//#include "../../src/yuv/yuv420.h"
-//#include "../../src/yuv/nv21.h"
-//#include "../../src/yuv/bgra8888.h"
+
 #include "../../src/yuv_ffi.h"
 #include "../../src/yuv_ffi.c"
 #include "../../src/yuv/gauss.c"
